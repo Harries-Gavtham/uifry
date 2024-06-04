1d215d2fe6b12ec2a3ea4000aa308b3e89f03f5e
Harries-Gavtham <harriesjj77@gmail.com>
2024-06-04 20:19:20 +0530

    navbar completed yet to add button functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default App;
